4d5e58b9486c89dd666a2d40eeb9259320742b15
ragul28 <ragulh28@gmail.com>
2020-10-04 22:10:40 +0530

    Fix the module path

--- aws/main.tf
+++ aws/main.tf
@@ -1,11 +1,11 @@
 module "aws-vpc" {
-  source         = "./modules/aws/vpc"
+  source         = "./modules/vpc"
   project        = var.project
   vpc_cidr_block = var.vpc_cidr_block
 }
 
 module "aws-eks" {
-  source         = "./modules/aws/eks"
+  source         = "./modules/eks"
   project        = var.project
   eks_subnet_ids = module.aws-vpc.subnet_ids
   eks_vpc_id     = module.aws-vpc.vpc_id
