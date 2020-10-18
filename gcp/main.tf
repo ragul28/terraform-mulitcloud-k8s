00e9d0c239a3accfc1ab2a577e0dc14e01dbb21d
Ragulan <ragulan@arimaclanka.com>
2020-10-18 14:46:40 +0530

    Update gke module with private cluster support

--- gcp/main.tf
+++ gcp/main.tf
@@ -6,13 +6,15 @@ module "gcp-vpc" {
 }
 
 module "gcp-gke" {
-  source        = "./modules/gke"
-  project       = var.project
-  gcp_region    = var.gcp_region
-  gke_subnet    = module.gcp-vpc.gke_subnet
-  gke_vpc       = module.gcp-vpc.gke_vpc
-  instance_type = var.instance_type
-  node_count    = var.node_count
-  gke_username  = var.gke_username
-  gke_password  = var.gke_password
+  source                        = "./modules/gke"
+  project                       = var.project
+  gcp_region                    = var.gcp_region
+  gcp_zone                      = var.gcp_zone
+  gke_vpc                       = module.gcp-vpc.gke_vpc
+  gke_subnet                    = module.gcp-vpc.gke_subnet
+  gke_subnet_secondary_ip_range = module.gcp-vpc.gke_subnet_secondary_ip_range
+  instance_type                 = var.instance_type
+  node_count                    = var.node_count
+  gke_username                  = var.gke_username
+  gke_password                  = var.gke_password
 }
