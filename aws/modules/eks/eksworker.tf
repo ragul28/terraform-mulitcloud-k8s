492b8804513bc7fae525fb513323ed27a327ca44
ragul28 <ragulh28@gmail.com>
2020-12-12 23:58:27 +0530

    Added support for eks spot worker node

--- aws/modules/eks/eksworker.tf
+++ aws/modules/eks/eksworker.tf
@@ -45,10 +45,42 @@ resource "aws_eks_node_group" "worker-node" {
 
   scaling_config {
     desired_size = var.node_count
+    max_size     = var.node_count
+    min_size     = 1
+  }
+
+  lifecycle {
+    ignore_changes = [scaling_config[0].desired_size]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+
+resource "aws_eks_node_group" "worker-node-spot" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "worker-nodes-spot"
+  capacity_type   = "SPOT"
+  # release_version = "1.18"
+  instance_types  = var.instance_types 
+  disk_size       = var.disk_size
+  
+  node_role_arn   = aws_iam_role.node.arn
+  subnet_ids      = var.eks_subnet_ids
+
+  scaling_config {
+    desired_size = var.node_count
     max_size     = (var.node_count * 2) - 1 
     min_size     = 1
   }
 
+  lifecycle {
+    ignore_changes = [scaling_config[0].desired_size]
+  }
+  
   depends_on = [
     aws_iam_role_policy_attachment.node-AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.node-AmazonEKS_CNI_Policy,
